a0f849ed19d7fdb65ae9de878a9845a89eb43924
Becky Hall <uncoolplane@gmail.com>
2017-03-07 19:59:17 -0700

    working copy

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+var port = 3000;
+
+var messages = [];
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json(
+    {
+      messages: messages
+    }
+  );
+});
+
+app.post('/messages', function(req, res, next) {
+  {
+    console.log(req.body.message);
+    messages.push({
+      message: req.body.message,
+      time: new Date()});
+    res.status(200).json({"messages":messages});
+  }
+});
+
+app.listen(port, function() {
+  console.log("Listening on port " + port);
+})
